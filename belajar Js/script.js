4670355f5e1e5c982c29e3653ad22df9b7015804
Johanda Putra <Johanda201@gmail.com>
2022-10-12 10:36:45 +0700

    Challenges html pertama

---
+++ belajar Js/script.js
@@ -0,0 +1,30 @@
+// let name = '';
+
+// function myFunction (p1, p2) {
+//     return p1 * p2;
+// }
+
+
+// function multiplyFunc (p1, p2) {
+//     return p1 * p2;
+// }
+
+
+// function onlyProcess (p1, p2) {
+//     return p1 * p2;
+// }
+
+// console.log(myFunction());
+
+// console.log(multiplyFunc(4,12));
+
+// const list = document.getElementById(list);
+
+// addEventListener ("submit", (event) => ){
+//     const  onsumbit 
+//     event.preventDefault();
+//     const {nama, email} = event.target;
+
+//     console.log(nama.value);
+//     console.log(email.value);
+// }
